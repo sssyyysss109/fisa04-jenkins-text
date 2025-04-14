b7f5b3f51eecad1c52b2dca20c6c468f5cedf269
sssyyysss109 <sunyu0323@gmail.com>
2025-04-14 15:06:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 # .env 불러오기
 load_dotenv()
 
-myDBid = os.environ.get('DBid')
-myDBpw = os.environ.get('DBpassword')
-mySecretKey = os.environ.get('FLASK_SECRET_KEY')
-myDB = os.environ.get('DB')
-mySchema = os.environ.get('SCHEMA')
+myDBid = os.environ.get('DB_USER')
+myDBpw = os.environ.get('DB_PASSWORD')
+mySecretKey = os.environ.get('SECRET_KEY')
+myDB = os.environ.get('DB_HOST')
+mySchema = os.environ.get('DB_NAME')
 
 # db를 저장할 폴더/파일이름 
 BASE_DIR = os.path.dirname(__file__)
@@ -16,4 +16,4 @@ BASE_DIR = os.path.dirname(__file__)
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'test.db'))
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{myDBid}:{myDBpw}@{myDB}:3306/{mySchema}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY= mySecretKey
+FLASK_SECRET_KEY= mySecretKey
